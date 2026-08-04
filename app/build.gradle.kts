2c26b51af9b15acbdf459fdd5b788aef1ab2f42b
hgwebs4 <hgwebs4@gmail.com>
2026-08-04 23:27:01 +0530

    Update build.gradle.kts via HGits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "")
+            storeFile = System.getenv("KEYSTORE_FILE")?.let { java.io.File(it) }
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
